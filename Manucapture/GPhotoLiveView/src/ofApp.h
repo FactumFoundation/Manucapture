c10880380fdc458c89d87bb1be82df1fe658164f
jcanofuentes <jcanofuentes@gmail.com>
2018-05-17 15:37:11 +0200

    Liveview stops cameras when finish

--- Manucapture/GPhotoLiveView/src/ofApp.h
+++ Manucapture/GPhotoLiveView/src/ofApp.h
@@ -39,6 +39,7 @@ public:
     void windowResized(int w, int h);
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
+    void exit();
 
     ofxOscReceiver receiver;
     ofxOscSender sender;
